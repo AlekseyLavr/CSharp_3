24605cc06457207d3284930ec559c2c88a17347c
AlekseyLavr <govola77@gmail.com>
2023-07-15 16:11:09 +0300

    Домашнее задание № 19

---
+++ DZ_S3_19/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int revers = 0;
+int num = 0;
+int rest = 0;
+if (number > 9999 && number < 100000)
+{
+    for (num = number; number != 0; number = number / 10)
+    {
+        rest = number % 10;
+        revers = revers * 10 + rest;
+    }
+}
+if (num == revers)
+    Console.Write($"{num} является палиндромом");
+else if (num != revers)
+    Console.Write($"{num} не является палиндромом");
